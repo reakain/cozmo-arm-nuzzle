b01ed2ce67606bbe6f466a0f4af0c3c086d2962b
Reakain <reakain@users.noreply.github.com>
2020-10-26 16:37:08 -0700

    added coded for read image test, integrated read image stuff into main functionality and class, so... should technically be doing everything?

--- im_detect.py
+++ im_detect.py
@@ -3,10 +3,16 @@
 import cv2 as cv
 
 class FrameAnalysis:
-    def __init__(self, image):
-        self.image = image
+    def __init__(self,  threshold1 = 100, threshold2 = 200):
+        self.threshold1 = threshold1
+        self.threshold2 = threshold2
 
-    def find_edges(self, threshold1 = 100, threshold2 = 200):
+    def new_image(self, cli, image):
+        del cli
+        self.image = image
+        self.find_edges()
+
+    def find_edges(self):
         # Canny edge detection with threshold settings to start...
-        self.edges = cv.Canny(img,threshold1,threshold2)
+        self.edges = cv.Canny(self.image,self.threshold1,self.threshold2)
         return self.edges
--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 
 import cv2 as cv
+import time
 import pycozmo
 from im_detect import FrameAnalysis
 
-# Setup pycozmo and cozmo
 
-# Get frame from cozmo
-frame = FrameAnalysis(img_cozmo)
+# pycozmo program
+def pycozmo_program(cli: pycozmo.client.Client):
+    # spin up the Frame Analysis stuff
+    frame_analysis = FrameAnalysis()
 
-# Analyze frame and get edges
-result = frame.find_edges()
+    angle = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
+    cli.set_head_angle(angle)
 
-# Ouput frame
-plt.subplot(121),plt.imshow(frame.image,cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(frame.edges,cmap = 'gray')
-plt.title('Edge'), plt.xticks([]), plt.yticks([])
+    pkt = pycozmo.protocol_encoder.EnableCamera()
+    cli.conn.send(pkt)
+    pkt = pycozmo.protocol_encoder.EnableColorImages(enable=True)
+    cli.conn.send(pkt)
 
-plt.show()
+    # Wait for image to stabilize.
+    time.sleep(2.0)
+
+    # handler to get image and feed to frame analysis
+    cli.add_handler(pycozmo.event.EvtNewRawCameraImage, frame_analysis.new_image, one_shot=True)
+
+    # Wait for image to be captured.
+    time.sleep(1)
+
+    # Ouput frame and edges
+    plt.subplot(121),plt.imshow(frame_analysis.image,cmap = 'gray')
+    plt.title('Original'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(frame_analysis.edges,cmap = 'gray')
+    plt.title('Edge'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
+
+
+pycozmo.run_program(pycozmo_program)
+
+
