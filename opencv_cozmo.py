41399e722399be6d51d23b597c25a2fb51087b51
Reakain <reakain@users.noreply.github.com>
2020-11-09 14:55:56 -0800

    added ground check and swap to swap between edge, yolo, and ground check

--- opencv_cozmo.py
+++ opencv_cozmo.py
@@ -11,8 +11,12 @@ import pycozmo
 import pygame
 
 import numpy as np
-import cv2 as cv
+import cv2
 
+from yolo import YOLO
+
+# ground, yolo, edge
+analysis_method = "ground"
 
 
 # Last image, received from the robot.
@@ -46,9 +50,57 @@ def cv2ImageToSurface(cv2Image):
     surface = pygame.image.frombuffer(cv2Image.flatten(), size, format)
     return surface.convert_alpha() if format == 'RGBA' else surface.convert()
 
+# Taken from https://github.com/BigFace83/BFRMR1
+def CheckGround(img):
+
+    StepSize = 8
+    EdgeArray = []
+
+    #time.sleep(0.1)#let image settle
+    #ret,img = capture.read() #get a bunch of frames to make sure current frame is the most recent
+    #ret,img = capture.read() 
+    #ret,img = capture.read()
+    #ret,img = capture.read()
+    #ret,img = capture.read() #5 seems to be enough
+
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)   #convert img to grayscale and store result in imgGray
+    imgGray = cv2.bilateralFilter(imgGray,9,30,30) #blur the image slightly to remove noise             
+    imgEdge = cv2.Canny(imgGray, 50, 100)             #edge detection
+    
+    imagewidth = imgEdge.shape[1] - 1
+    imageheight = imgEdge.shape[0] - 1
+    
+    for j in range (0,imagewidth,StepSize):    #for the width of image array
+        for i in range(imageheight-5,0,-1):    #step through every pixel in height of array from bottom to top
+                                               #Ignore first couple of pixels as may trigger due to undistort
+            if imgEdge.item(i,j) == 255:       #check to see if the pixel is white which indicates an edge has been found
+                EdgeArray.append((j,i))        #if it is, add x,y coordinates to ObstacleArray
+                break                          #if white pixel is found, skip rest of pixels in column
+        else:                                  #no white pixel found
+            EdgeArray.append((j,0))            #if nothing found, assume no obstacle. Set pixel position way off the screen to indicate
+                                               #no obstacle detected
+            
+    
+    for x in range (len(EdgeArray)-1):      #draw lines between points in ObstacleArray 
+        cv2.line(img, EdgeArray[x], EdgeArray[x+1],(0,255,0),1) 
+    for x in range (len(EdgeArray)):        #draw lines from bottom of the screen to points in ObstacleArray
+        cv2.line(img, (x*StepSize,imageheight), EdgeArray[x],(0,255,0),1)
+
+    return img
+
+
+    #if DisplayImage is True:
+    #    cv2.imshow("camera", img)
+    #    cv2.waitKey(10)
+
+
 def pycozmo_program(cli: pycozmo.client.Client):
 
-    global last_im, updated
+    global last_im, updated, analysis_method
+
+    if(analysis_method == "yolo"):
+        yolo = YOLO("./yolo-coco/coco.names","./yolo-coco/yolov3.weights",
+    "./yolo-coco/yolov3.cfg",0.5,0.3)
 
     # activate the pygame library . 
     # initiate pygame and give permission 
@@ -73,7 +125,7 @@ def pycozmo_program(cli: pycozmo.client.Client):
 
     # Raise head.
     angle = (pycozmo.robot.MAX_HEAD_ANGLE.radians - pycozmo.robot.MIN_HEAD_ANGLE.radians) / 2.0
-    cli.set_head_angle(angle)
+    cli.set_head_angle(0)#angle)
 
     pkt = pycozmo.protocol_encoder.EnableCamera()
     cli.conn.send(pkt)
@@ -90,7 +142,7 @@ def pycozmo_program(cli: pycozmo.client.Client):
     #pkt = pycozmo.protocol_encoder.EnableCamera()
     #cli.conn.send(pkt)
 
-    fgbg = cv.createBackgroundSubtractorMOG2(
+    fgbg = cv2.createBackgroundSubtractorMOG2(
     history=10,
     varThreshold=2,
     detectShadows=False)
@@ -101,38 +153,51 @@ def pycozmo_program(cli: pycozmo.client.Client):
 
             # Get last image.
             im = last_im
-            im2 = im.copy()
-            gray = cv.cvtColor(im2, cv.COLOR_BGR2GRAY)
             updated = False
-
-            # filtering https://www.sicara.ai/blog/2019-03-12-edge-detection-in-opencv
-            filtered = cv.bilateralFilter(gray, 7, 50, 50)
-            foreground = fgbg.apply(filtered)
-
-            kernel = np.ones((50,50),np.uint8)
-            foreground = cv.morphologyEx(foreground, cv.MORPH_CLOSE, kernel)
-
-            # edge detection
-            edges = cv.Canny(filtered,100,200)
-
-            # Crop off moving area
-            cropped = (foreground //255) * edges
-
-            # convex hull from edge detection
-            contours, hierarchy = cv.findContours(edges, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-            for c in contours:
-                hull = cv.convexHull(c)
-                cv.drawContours(im2, [hull], 0, (0, 255, 0), 2)
 
             # completely fill the surface object 
             # with white colour 
             display_surface.fill(white) 
+
+            if(analysis_method == "yolo"):
+                detect_im = yolo.analyze_image(last_im.copy())
+                im_both = np.vstack((last_im, detect_im))
+            
+            elif(analysis_method == "ground"):
+                grnd = CheckGround(last_im.copy())
+                im_both = np.vstack((last_im, grnd))
+
+            elif(analysis_method == "edge"):
+                im2 = last_im.copy()
+                gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+                # filtering https://www.sicara.ai/blog/2019-03-12-edge-detection-in-opencv
+                filtered = cv2.bilateralFilter(gray, 7, 50, 50)
+                foreground = fgbg.apply(filtered)
+
+                kernel = np.ones((50,50),np.uint8)
+                foreground = cv2.morphologyEx(foreground, cv2.MORPH_CLOSE, kernel)
+
+                # edge detection
+                edges = cv2.Canny(filtered,100,200)
+
+                # Crop off moving area
+                cropped = (foreground //255) * edges
+
+                # convex hull from edge detection
+                contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+                for c in contours:
+                    hull = cv2.convexHull(c)
+                    cv2.drawContours(im2, [hull], 0, (0, 255, 0), 2)
+                
+                im_both = np.vstack((last_im, im2))
+            
         
             # copying the image surface object 
             # to the display surface object at 
             # (0, 0) coordinate. 
-            display_surface.blit(cv2ImageToSurface(im), (0, 0)) 
-            display_surface.blit(cv2ImageToSurface(cropped), (0,240))
+            display_surface.blit(cv2ImageToSurface(im_both), (0, 0)) 
+            #display_surface.blit(cv2ImageToSurface(grnd), (0,240))
             # Draws the surface object to the screen.   
             pygame.display.update()  
 
